bc48cb44aec1c56e5bcbc260469a40871880e1ab
Acer Aspire 3 <Acer Aspire 3@DESKTOP-3O0TCLQ>
2023-05-02 22:59:04 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿switch (position)
+{
+    case "Assistant Developer";
+        salary *= 1.05f;
+        break;
+    case "IT Support";
+        salary *= 1.07f;
+        break;
+    case "Senior Developer";
+        salary *= 1.10f;
+        break;
+    case "Administrator";
+        salary *= 1.08f;
+        break;
+    case "Other";
+        salary *= 1.03f;
+        break;
+}
+
+Console.WriteLine($"Your Salary = {Math.Round(salary)}");
